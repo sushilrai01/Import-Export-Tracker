a5689ab3db2fee79bb70fbb4749d7a4efde433ce
madmdx07 <mahessdhant@gmail.com>
2024-04-07 17:45:55 +0545

    DbOptions added

---
+++ ImportExportTracker.DB/DbOptions.cs
@@ -0,0 +1,20 @@
+﻿using ImportExportTracker.DB.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImportExportTracker.DB
+{
+    public class DbOptions : IDbOptions
+    {
+        public DbContextOptions<ImportExportDbContext> ConOptions { get; set; }
+        public DbOptions(DbContextOptions<ImportExportDbContext> uoptions)
+        {
+            ConOptions = uoptions;
+        }
+    }
+    public interface IDbOptions
+    {
+        DbContextOptions<ImportExportDbContext> ConOptions { get; set; }
+
+    }
+}
+
