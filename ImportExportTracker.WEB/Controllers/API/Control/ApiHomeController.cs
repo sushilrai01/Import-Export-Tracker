996920ed21e76bfddbbcb1561b6d3aae82fda78b
sushilrai01 <ssilray16@gmail.com>
2024-04-14 23:05:02 +0545

    api: Home import report

---
+++ ImportExportTracker.WEB/Controllers/API/Control/ApiHomeController.cs
@@ -0,0 +1,24 @@
+﻿using ImportExportTracker.SERVICES.Control.HomeServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImportExportTracker.WEB.Controllers.API.Control
+{
+    [Route("api/home")]
+    [ApiController]
+    public class ApiHomeController : ControllerBase
+    {
+        IHomeServices _actionServices;
+        public ApiHomeController(IHomeServices actionServices)
+        {
+            _actionServices = actionServices;
+        }
+
+        [HttpGet("topFiveRecords")]
+        public async Task<IActionResult> Index()
+        {
+            var serviceResponse = await _actionServices.ListImportExport();
+            return Ok(serviceResponse);
+        }
+    }
+}
