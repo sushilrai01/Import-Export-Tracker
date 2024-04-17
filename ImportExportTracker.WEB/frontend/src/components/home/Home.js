673f52046a3c160a7e28ba989c58fe5520eb2753
sushilrai01 <ssilray16@gmail.com>
2024-04-17 23:57:20 +0545

    top imports homepage

--- ImportExportTracker.WEB/frontend/src/components/home/Home.js
+++ ImportExportTracker.WEB/frontend/src/components/home/Home.js
@@ -1,12 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TopImportsTable from "./TopImportsTable";
+import { LineGraph } from "./LineGraph";
+import apiUrl from "../../common/app-url";
+import axios from "axios";
 
 export default function Home() {
+  //variables to hold TOP 5 commodities import
+  const [topCommodities, setTopCommodities] = useState();
+  const [topCommodities11, setTopCommodities11] = useState();
+
+  //Calling API to fetch data of TOP Commodities
+
+  useEffect(() => {
+    axios
+      .get(apiUrl.apiHomeUrl.getTopCommodities)
+      .then((response) => {
+        console.log("HEllo");
+        console.log(response.data.data.categoryReportList);
+        console.log(response.data.data.commodityReportList);
+        setTopCommodities(response.data.data.categoryReportList);
+        setTopCommodities11(response.data.data.commodityReportList);
+      })
+      .catch((error) => {
+        console.error("error: ", error);
+      });
+  }, []);
+
   return (
     <>
       <div className="container">
         <div className="text-justify">
-          <p className="col-md-6 ">
+          <p className="col-md-12 ">
             Lorem Lorem Lorem Lorem Lorem Lorem Lorem ipsum dolor sit amet
             consectetur adipisicing elit. Dolorum veritatis illum quae
             temporibus, asperiores minus, voluptas maiores, earum blanditiis
@@ -14,7 +38,13 @@ export default function Home() {
             modi voluptas voluptatem dicta?
           </p>
         </div>
-        <TopImportsTable />
+
+        <LineGraph />
+
+        <TopImportsTable
+          topItems={topCommodities}
+          topItems11={topCommodities11}
+        />
       </div>
     </>
   );
--- ImportExportTracker.WEB/frontend/src/components/home/TopImportsTable.js
+++ ImportExportTracker.WEB/frontend/src/components/home/TopImportsTable.js
@@ -1,9 +1,75 @@
 import React from "react";
 
-export default function TopImportsTable() {
+export default function TopImportsTable(props) {
+  const { topItems, topItems11 } = props;
   return (
     <>
-      <div>HELLOLOLOLOLOLO</div>
+      <div className="row my-3">
+        <div className="card">
+          <div className="col-md-12">
+            <h4>TOP 5 IMPORT BY CATEGORY</h4>
+            <div>
+              <table className="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>SN</th>
+                    <th>Category</th>
+                    <th>Fiscal Year</th>
+                    <th>Total Quantity</th>
+                    <th>Total Import Value</th>
+                    <th>Total Import Revenue</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {topItems.map((cat, i) => (
+                    <tr key={i}>
+                      <td>{1 + i}</td>
+                      <td>{cat.categoryTitle}</td>
+                      <td>{cat.fiscalYearTitle}</td>
+                      <td>{cat.totalQuantity}</td>
+                      <td>{cat.totalImportValue}</td>
+                      <td>{cat.totalImportRevenue}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div className="card my-3">
+          <div className="col-md-12">
+            <h4>TOP 5 IMPORT BY COMMODITY</h4>
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>SN</th>
+                  <th>Category</th>
+                  <th>Commodity Name</th>
+                  <th>HSCODE</th>
+                  <th>Fiscal Year</th>
+                  <th>Total Quantity</th>
+                  <th>Total Import Value</th>
+                  <th>Total Import Revenue</th>
+                </tr>
+              </thead>
+              <tbody>
+                {topItems11.map((item, i) => (
+                  <tr key={i}>
+                    <td>{1 + i}</td>
+                    <td>{item.categoryTitle}</td>
+                    <td>{item.commodityName}</td>
+                    <td>{item.hsCode}</td>
+                    <td>{item.fiscalYearTitle}</td>
+                    <td>{item.totalQuantity}</td>
+                    <td>{item.totalImportValue}</td>
+                    <td>{item.totalImportRevenue}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
